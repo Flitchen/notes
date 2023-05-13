dc49657dbe679eb2f588e086626ad862d02654c1
Flitchen <abdulkarimabbas088@gmail.com>
2023-05-13 11:45:56 +0300

    Changing info alert to danger

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -12,7 +12,7 @@ function News() {
             < div class="alert alert-danger mt-3 text-center">
                 Deadline for submitting group assignment of Information System Management will be on <u>2<sup>nd</sup> June 2023</u>
             </div>
-            < div class="alert alert-info mt-3 text-center">
+            < div class="alert alert-danger mt-3 text-center">
                 Deadline for submitting group assignment of Information System Management will be on <u>15<sup>th</sup> May 2023</u>. The assignment should be done with not more than 4 people
             </div>
 
