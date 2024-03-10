29e9eb1b174f9e6a190c9cb0f17c0e471ceb50f9
Flitchen <104328567+Flitchen@users.noreply.github.com>
2024-03-10 09:37:23 +0300

    Update Assignments

--- src/components/Assignments.jsx
+++ src/components/Assignments.jsx
@@ -4,6 +4,8 @@ function Assignments() {
     return (
         <div className="container">
             <div className="display-5 text-center my-4">Assignments</div>
+            < div class=" mt-3 text-center">
+Previous Assignments            </div>
             <div className="list-group list-group-numbered list-group-flush mx-5 mb-3">
 
                 <a href="L3 (chapter 4) Group work-and-presentation.pptx" class="list-group-item list-group-item-action">Fundamentals of Artificial Intelligence (Group Assignment)</a>
